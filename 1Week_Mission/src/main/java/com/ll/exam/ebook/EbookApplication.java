26fcb3adc7acee763a05fc32f8b6a60baee2c9d2
JI-NAMEUN <skadms1720@gmail.com>
2022-10-19 17:51:32 +0900

    Controllor 및 Service 구현, 회원가입 기능

--- 1Week_Mission/src/main/java/com/ll/exam/ebook/EbookApplication.java
+++ 1Week_Mission/src/main/java/com/ll/exam/ebook/EbookApplication.java
@@ -3,6 +3,7 @@ package com.ll.exam.ebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class EbookApplication {
