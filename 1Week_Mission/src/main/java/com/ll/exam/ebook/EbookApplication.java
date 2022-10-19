1e595d511e4f74f3c4a431d078db97b10be2604e
JI-NAMEUN <skadms1720@gmail.com>
2022-10-19 17:01:32 +0900

    initData 도입

--- 1Week_Mission/src/main/java/com/ll/exam/ebook/EbookApplication.java
+++ 1Week_Mission/src/main/java/com/ll/exam/ebook/EbookApplication.java
@@ -2,7 +2,8 @@ package com.ll.exam.ebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
 @SpringBootApplication
 public class EbookApplication {
 
--- 1Week_Mission/src/test/java/com/ll/exam/ebook/EbookApplicationTests.java
+++ 1Week_Mission/src/test/java/com/ll/exam/ebook/EbookApplicationTests.java
@@ -2,7 +2,7 @@ package com.ll.exam.ebook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootTest
 class EbookApplicationTests {
 
